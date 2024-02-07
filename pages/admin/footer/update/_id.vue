027696154aef795f69dcd87261f9a47ab5fb4b55
LongPham0302 <bekeo0302@gmail.com>
2024-02-07 18:11:58 +0700

    fix bug

--- pages/admin/footer/update/_id.vue
+++ pages/admin/footer/update/_id.vue
@@ -1,63 +1,109 @@
 <template>
-    <div class="col-span-10 mt-20 ml-10">
-        <label class="block mb-1 text-sm" for="CompanyName">Company Name:</label>
-        <input v-model="companyName" id="CompanyName"
-            class="w-3/5 border px-4 py-2 rounded mb-4 focus:border-blue-500 focus:shadow-outline outline-none" type="text"
-            autofocus placeholder="CompanyName..." />
+  <div class="col-span-10 mt-5 ml-10 relative">
+    <label class="block mb-1 text-sm" for="companyLocation"
+      >companyLocation:</label
+    >
+    <textarea
+      v-model="companyLocation"
+      id="companyLocation"
+      class="w-3/5 border px-4 py-2 rounded mb-4 focus:border-blue-500 focus:shadow-outline outline-none"
+      type="text"
+      autofocus
+      placeholder="companyLocation..."
+    ></textarea>
 
-        <label class="block mb-1 mt-2 text-sm" for="SocialMedia">Social Media:</label>
-        <input v-model="socialMedia" id="SocialMedia"
-            class="w-3/5 border px-4 py-2 rounded mb-4 focus:border-blue-500 focus:shadow-outline outline-none" type="text"
-            autofocus placeholder="SocialMedia..." />
+    <label class="block mb-1 mt-2 text-sm" for="customerSupport"
+      >customerSupport:</label
+    >
+    <textarea
+      v-model="customerSupport"
+      id="customerSupport"
+      class="w-3/5 h-1/4 border px-4 py-2 rounded mb-4 focus:border-blue-500 focus:shadow-outline outline-none"
+      type="text"
+      autofocus
+      placeholder="customerSupport..."
+    ></textarea>
 
-        <label class="block mb-1 mt-2 text-sm" for="YearRelease">Year Release:</label>
-        <input v-model="yearRelease" id="YearRelease"
-            class="w-3/5 border px-4 py-2 rounded mb-4 focus:border-blue-500 focus:shadow-outline outline-none" type="number"
-            autofocus placeholder="YearRelease..." />
+    <label class="block mb-1 mt-2 text-sm" for="policyRegulations"
+      >policyRegulations:</label
+    >
+    <textarea
+      v-model="policyRegulations"
+      id="policyRegulations"
+      class="w-3/5 h-2/5 border px-4 py-2 rounded mb-4 focus:border-blue-500 focus:shadow-outline outline-none"
+      type="text"
+      autofocus
+      placeholder="policyRegulations..."
+    ></textarea>
 
-        <span v-if="error" class="font-medium">Info alert!</span> {{ error }}
+    <span v-if="error" class="font-medium">Info alert!</span> {{ error }}
 
-        <br />
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800  rounded p-3 mt-4" @click="updateFooter()">Update</button>
-    </div>
+    <br />
+    <button
+      type="button"
+      class="absolute top-0 right-5 text-white bg-blue-700 hover:bg-blue-800 rounded p-3 mt-4"
+      @click="updateFooter()"
+    >
+      Update
+    </button>
+  </div>
 </template>
+
 <script>
 export default {
-    layout: "admin",
-    data() {
-        return {
-            companyName: "",
-            socialMedia: "",
-            yearRelease: null,
-            error: "",
-        }
-    },
-    created() {
-        this.getFooterById();
-    },
-    methods: {
-        updateFooter() {
-            let id = this.$route.params.id;
-            let data = {
-                companyName: this.companyName,
-                socialMedia: this.socialMedia,
-                yearRelease: this.yearRelease
-            }
-            this.$store.dispatch("updateFooter", {id, data}).then(res => {
-                if(res.ok) {
-                    return this.$router.replace("/admin/footer");
-                }
-            })
-        },
+  layout: "admin",
+  data() {
+    return {
+      companyLocation: "",
+      customerSupport: "",
+      policyRegulations: null,
+      error: "",
+    };
+  },
+  created() {
+    this.getFooterById();
+  },
 
-        getFooterById() {
-            let id = this.$route.params.id;
-            this.$store.dispatch("getFooterById", id).then(res => {
-                this.companyName = res.companyName;
-                this.socialMedia = res.socialMedia;
-                this.yearRelease = res.yearRelease;
-            })
+  methods: {
+    async updateFooter() {
+      try {
+        let id = this.$route.params.id;
+        let data = {
+          companyLocation: this.companyLocation.split("\n"),
+          customerSupport: this.customerSupport.split("\n"),
+          policyRegulations: this.policyRegulations.split("\n"),
+        };
+        console.log("data", data);
+        const response = await this.$store.dispatch("updateFooter", {
+          id,
+          data,
+        });
+        if (response.ok) {
+          this.$router.replace("/admin/footer");
         }
-    }
-}
+      } catch (error) {
+        console.error("Error updating footer:", error);
+        this.error = "Error updating footer. Please try again.";
+      }
+    },
+
+    async getFooterById() {
+      try {
+        let id = this.$route.params.id;
+        const res = await this.$store.dispatch("getFooterById", id);
+        this.companyLocation = res.companyLocation.join("\n");
+        this.customerSupport = res.customerSupport.join("\n");
+        this.policyRegulations = res.policyRegulations.join("\n");
+      } catch (error) {
+        console.error("Error fetching footer:", error);
+        this.error = "Error fetching footer. Please try again.";
+      }
+    },
+
+    formatArrayToString(array) {
+      return array ? array.join("\n") : "";
+    },
+
+  },
+};
 </script>
