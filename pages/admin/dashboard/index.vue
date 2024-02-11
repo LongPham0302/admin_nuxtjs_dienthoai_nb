b938ce8be0e8c7f260f8e330bf178e3b64df4289
LongPham0302 <72117403+LongPham0302@users.noreply.github.com>
2024-02-11 11:24:45 +0700

    Feature/addimgproduct (#11)
    * push code
    
    * add code

---
+++ pages/admin/dashboard/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <DashboardAdminComponent  />
+</template>
+
+<script>
+import DashboardAdminComponent  from "@components/admin/dashboard";
+export default {
+  name: "DashboardAdmin",
+  layout: "admin",
+  components: {
+    DashboardAdminComponent ,
+  },
+};
+</script>
+
+<style scoped>
+/* Thêm các quy tắc CSS Tailwind tại đây nếu cần */
+</style>
